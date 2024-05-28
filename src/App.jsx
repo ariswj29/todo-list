7141f958b1b55d2c2556ad41526e1de66bac41da
ariswj29 <ariswj29@gmail.com>
2024-05-29 01:24:13 +0700

    feat: configuration tailwind, add a skeleton first

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
-
+const styleSection1 = "text-center p-4 m-0-auto"
   return (
     <>
-      <h1 className='text-gray-700'>Todo List App</h1>
+      <main className="py-10">
+        <section className={styleSection1}>
+          <h1 className='text-3xl mb-8'>Chores ToDo List</h1>
+          <div className="flex justify-between"> 
+            <input className='border-green-500' type='checkbox' id='toggle' />
+            <label htmlFor='toggle'>Show completed chores</label>
+            <button className='p-1 w-10 h-10 border-[1px] border-solid border-red-400 text-white rounded'>X</button>
+          </div>
+        </section>
+        <hr />
+        <section className={styleSection1}>
+          <div className="flex flex-col">
+            <h2 className="text-2xl mb-4">Done :</h2>
+            <form className='flex flex-col items-center'>
+            <label className='m-2' htmlFor="input">Add Todo</label>
+              <input className='mb-4 p-2 bg-transparent border-[1px] border-solid border-gray-600 rounded' type="text" id="input" />
+              <button className='px-4 w-fit h-10 rounded-md text-black bg-sky-400' type="submit">Add Task</button>
+            </form>
+          </div>
+        </section>
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    const style = "text-center p-4 fixed bottom-0 w-full"
+    return (
+      <footer className={style}>
+        <p>&copy; 2024 by Aris Wildan Jannatan</p>
+      </footer>
+    )
+}
+
+export default Footer;
